5feedcf1eebc3c94dba70aa191b258a958479e01
TDStrasser <43877585+TDStrasser@users.noreply.github.com>
2024-04-01 10:47:17 +1100

    Fix to installability

--- service_worker.js
+++ service_worker.js
@@ -1,11 +1,22 @@
 const CACHE_NAME = `kookaterminal`;
 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_01';
+
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/'
+      '/',
+      '/manifest.json',
+      '/index.html',
+      '/kmgr.css',
+      '/service_worker.js',
+      '/term.js'
     ]);
   })());
 });
